b76ef142074cc5358fca7fc2518515513f6109fa
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2026-02-27 16:09:04 +0200

    Adjust settings

--- backend/bettermetesttask/settings.py
+++ backend/bettermetesttask/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -32,7 +33,8 @@ CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
 
 ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "clodev")
 
-ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "default_password_if_not_set")
+ADMIN_PASSWORD = os.environ.get(
+    "ADMIN_PASSWORD", "default_password_if_not_set")
 
 # Application definition
 
